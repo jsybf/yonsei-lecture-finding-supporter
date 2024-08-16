569385bfcc139765d24c5bd394f14b08ea74ba76
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2024-08-17 03:34:33 +0900

    jackson, entity-module, logger 의존성 추가

--- lecture-content/build.gradle.kts
+++ lecture-content/build.gradle.kts
@@ -11,7 +11,11 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.+")
     implementation(project(":scraping-batch"))
+    implementation(project(":entity-module"))
+    implementation("ch.qos.logback:logback-classic:1.5.6")
+    implementation("org.slf4j:slf4j-api:2.0.13")
 }
 
 tasks.test {
